f6045871a25bc4ca09f39fe394b077852b8cb8b2
Cherish <cherishkangwj@gmail.com>
2026-07-27 07:15:51 -0700

    feat: add RegistrationToken model

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import connectDB from './config/db';
 import cors from 'cors';
 import createApolloMiddleware from './graphql/server'
 import { User } from './models/User'
+import { RegistrationToken } from './models/RegistrationToken';
 
 dotenv.config();
 const app = express();
@@ -23,6 +24,14 @@ async function startServer(){
     app.get('/', (req, res) => res.send('Server is running'));
 
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+    const testToken = await RegistrationToken.create({
+        name: '测试员工',
+        invitedEmail: 'newhire@example.com',
+        token: 'abc123test',
+        expireAt: new Date(Date.now() + 3 * 60 * 60 * 1000), // 3小时后
+    });
+    console.log(testToken);
 
 }
 
