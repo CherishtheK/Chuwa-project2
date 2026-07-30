a6adb55a5ddc1521ef9e3a79c12e1b6cf8a021a3
Cherish <cherishkangwj@gmail.com>
2026-07-30 08:07:51 -0700

    feat: configure Apollo Client with authLink

--- client/src/app/apolloClient.ts
+++ client/src/app/apolloClient.ts
@@ -1,3 +1,25 @@
 // TODO: ApolloClient + authLink（从 localStorage 取 JWT 塞进 header）
 // 参考: https://www.apollographql.com/docs/react/networking/authentication
-export {};
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { SetContextLink } from "@apollo/client/link/context";
+
+const httpLink = new HttpLink({
+  uri: import.meta.env.VITE_GRAPHQL_URL
+});
+
+const authLink = new SetContextLink(({ headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
